c2a4822e841d08e44e1bd88134ec31c0c33ea54b
Liwaa Haddara <12786@coderacademy.edu.au>
2022-12-14 12:20:38 +1100

    created HomePageHeader component

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import HomePageHeader from "./components/HomePageHeader";
+
+function App() {
+  return <div className="App">
+    <HomePageHeader />
+  </div>;
+}
+
+
+export default App;
+
+
---
+++ react-portfolio/src/components/HomePageHeader.jsx
@@ -0,0 +1,10 @@
+function HomePageHeader() {
+    return (
+        <header id="Headings">
+        <h1 id="homeHeading">Liwaa</h1>
+        <h3 id="subHeading">Welcome..</h3>
+    </header>
+    )
+}
+
+export default HomePageHeader;
